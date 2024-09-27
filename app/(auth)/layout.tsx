b4877413d56d54bc9cc4004f769b07e7af08a6d3
AnilShebin <anilshebin@gmail.com>
2024-09-27 08:18:30 +0530

    Update Auth Image , now it will load faster
    Signed-off-by: AnilShebin <anilshebin@gmail.com>

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
       {children}
       <div className="auth-asset">
         <Image
-          src="/icons/auth-image.svg"
+          src="/icons/auth-image.png"
           alt="Auth image"
           width={500}
           height={500}
